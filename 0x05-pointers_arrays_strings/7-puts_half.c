0e21ec359d642a873e9f67efad3b81775b39d731
twiincarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-04-14 12:34:00 +0400

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,27 +7,14 @@
 void puts_half(char *str)
 {
 	int len = 0;
-	int start;
 	int i;
 
-	/* find length */
 	while (str[len] != '\0')
 	{
 		len++;
 	}
 
-	/* calculate starting point */
-	if (len % 2 == 0)
-	{
-		start = len / 2;
-	}
-	else
-	{
-		start = (len - 1) / 2 + 1;
-	}
-
-	/* print from start */
-	for (i = start; str[i] != '\0'; i++)
+	for (i = (len + 1) / 2; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
