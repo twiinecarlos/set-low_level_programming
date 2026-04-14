d4101bc8dda1338ec16c6ea221770566b83eea1c
twiincarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-04-14 17:09:28 +0400

    Create 3-strcmp.c

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	(void)s1;
+	(void)s2;
+	return (0);
+}
