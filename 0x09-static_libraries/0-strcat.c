4801824c3ba2c1f1618f497569f85e5390754c2c
twiincarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-04-14 17:07:44 +0400

    Create 0-strcat.c

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination
+ * @src: source
+ *
+ * Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	(void)src;
+	return (dest);
+}
