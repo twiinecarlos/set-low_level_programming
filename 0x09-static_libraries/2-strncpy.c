8c7dd920f9d8d71fe1e6fb2f1f702e2241de6d52
twiincarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-04-14 17:09:01 +0400

    Create 2-strncpy.c

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
+ *
+ * Return: pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	(void)src;
+	(void)n;
+	return (dest);
+}
