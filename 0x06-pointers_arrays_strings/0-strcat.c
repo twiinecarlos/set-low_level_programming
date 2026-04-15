dbe201a0540e02b1beec16df0d3ad8f2c647c319
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-04-15 11:00:08 +0400

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,21 +12,16 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int j = 0;
 
-	/* find end of dest */
-	while (dest[i] != '\0')
-	{
+	while (dest[i])
 		i++;
-	}
 
-	/* copy src into dest */
-	while (src[j] != '\0')
+	while (src[j])
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
 
-	/* add null terminator */
 	dest[i] = '\0';
 
 	return (dest);
