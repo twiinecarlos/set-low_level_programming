48597a9c7d64427733f0ad000546f7337c9cf1d3
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-04-15 11:17:45 +0400

    Create 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: input string
+ *
+ * Return: pointer to s
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = numbers[j];
+			}
+		}
+	}
+
+	return (s);
+}
