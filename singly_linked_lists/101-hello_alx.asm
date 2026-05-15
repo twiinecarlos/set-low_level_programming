bd2be5d4d618b47964ffbfe864c3ae013596822b
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-05-15 20:49:07 +0200

    Update 101-hello_alx.asm

--- singly_linked_lists/101-hello_alx.asm
+++ singly_linked_lists/101-hello_alx.asm
@@ -2,19 +2,16 @@ global main
 extern printf
 
 section .data
-    msg db "Hello, ALX", 10, 0
+    msg db "Hello, Holberton", 10, 0
 
 section .text
 main:
-    ; align stack for 64-bit ABI
     sub rsp, 8
 
-    ; call printf(msg)
     mov rdi, msg
     xor rax, rax
     call printf
 
-    ; restore stack and return
     add rsp, 8
     mov eax, 0
     ret
