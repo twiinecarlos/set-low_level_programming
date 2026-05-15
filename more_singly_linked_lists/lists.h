7803e6d45cd8a6516994ee1539a5b15bdf41345d
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-05-15 20:57:40 +0200

    Update lists.h

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -6,9 +6,7 @@
 /**
  * struct listint_s - singly linked list of integers
  * @n: integer value
- * @next: points to the next node
- *
- * Description: singly linked list node structure for integers
+ * @next: points to next node
  */
 typedef struct listint_s
 {
@@ -19,5 +17,6 @@ typedef struct listint_s
 /* function prototypes */
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 
 #endif
