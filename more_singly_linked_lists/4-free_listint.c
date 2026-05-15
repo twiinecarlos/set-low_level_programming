da822d270f069ed2ee6339f8b4073ef20134c59d
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-05-15 21:08:28 +0200

    Create 4-free_listint.c

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: head of the list
+ *
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
