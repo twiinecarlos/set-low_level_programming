c4ecf3e7ef49e0ca12e983d9befcf1cc25e0057e
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-05-15 21:10:31 +0200

    Add function to free listint_t linked list
    Implements a function to free a linked list and set the head to NULL.

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - frees a listint_t list and sets head to NULL
+ * @head: pointer to pointer to head of list
+ *
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL || *head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+
+	*head = NULL;
+} 
