6ff8d2e818fe6ed88c9365e3edbb55cbbdd31651
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-05-15 20:41:34 +0200

    Update lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -5,6 +5,9 @@
 
 /**
  * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
  */
 typedef struct list_s
 {
@@ -13,9 +16,10 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-/* prototypes */
+/* functions */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 
 #endif
