9d68513f11c8a6ff222b90eaac5f311f71e8a6d9
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-05-18 17:28:27 +0200

    Update 101-print_listint_safe.c

--- more_singly_linked_lists/101-print_listint_safe.c
+++ more_singly_linked_lists/101-print_listint_safe.c
@@ -3,39 +3,40 @@
 #include <stdlib.h>
 
 /**
- * print_listint_safe - prints a listint_t list safely
- * @head: pointer to head of list
+ * print_listint_safe - prints a listint_t list safely (even with loops)
+ * @head: pointer to list
  *
  * Return: number of nodes printed
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current, *check;
-	size_t count = 0;
+    const listint_t **seen;
+    size_t i = 0, count = 0;
+    const listint_t *current = head;
 
-	current = head;
+    seen = malloc(sizeof(listint_t *) * 1024 * 1024);
+    if (seen == NULL)
+        exit(98);
 
-	while (current != NULL)
-	{
-		check = head;
+    while (current != NULL)
+    {
+        for (i = 0; i < count; i++)
+        {
+            if (seen[i] == current)
+            {
+                printf("-> [%p] %d\n", (void *)current, current->n);
+                free(seen);
+                return (count);
+            }
+        }
 
-		while (check != current)
-		{
-			if (check == current->next)
-			{
-				printf("-> [%p] %d\n", (void *)current->next,
-					current->next->n);
-				return (count + 1);
-			}
+        seen[count] = current;
+        printf("[%p] %d\n", (void *)current, current->n);
+        count++;
 
-			check = check->next;
-		}
+        current = current->next;
+    }
 
-		printf("[%p] %d\n", (void *)current, current->n);
-
-		count++;
-		current = current->next;
-	}
-
-	return (count);
+    free(seen);
+    return (count);
 }
