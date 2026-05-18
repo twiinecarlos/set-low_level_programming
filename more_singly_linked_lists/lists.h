1148e552e69d8f397364c794bf115cfa5694edd8
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-05-18 17:24:25 +0200

    Update lists.h

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -14,7 +14,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* function prototypes */
+/* basic functions */
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -30,5 +30,6 @@ int delete_nodeint_at_index(listint_t **head,
 	unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
 
 #endif
