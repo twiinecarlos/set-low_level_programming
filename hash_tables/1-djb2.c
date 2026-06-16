64f624ddd9fc130e18641b8bb1935b75bfa82ccf
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-06-16 20:16:43 +0200

    Update 1-djb2.c

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,22 +1,12 @@
-#include "hash_tables.h"
-
-/**
- * hash_djb2 - Implements the djb2 hashing algorithm
- * @str: string to hash
- *
- * Return: resulting hash value
- */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
 	int c;
 
 	hash = 5381;
-
 	while ((c = *str++))
 	{
-		hash = ((hash << 5) + hash) + c;
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	}
-
 	return (hash);
 }
